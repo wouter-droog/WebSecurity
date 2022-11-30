b45979d1ec3173eda1a27d6017db402f05901d31
Wouter Droog <wdroog84@gmail.com>
2022-11-30 22:06:11 +0100

    Add login page

---
+++ WebAppIdentity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppIdentity.Pages.Account;
+
+public class Login : PageModel
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public Login(SignInManager<IdentityUser> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+    
+    [BindProperty]
+    public LoginViewModel LoginViewModel { get; set; }
+
+    
+    public void OnGet()
+    {
+        
+    }
+    
+    // async method to handle the login
+    public async Task<IActionResult> OnPostAsync()
+    {
+        // check if the model is valid
+        if (!ModelState.IsValid) return Page();
+        
+        var result = await _signInManager.PasswordSignInAsync(LoginViewModel.Email, LoginViewModel.Password, true, true);
+        
+        // check if the user is authenticated
+        if (result.Succeeded)
+        {
+            return RedirectToPage("/Index");
+        }
+
+        if (result.IsLockedOut)
+        {
+            ModelState.AddModelError("Login", "User account locked out.");
+            return Page();
+        }
+        
+        // if the user is not authenticated, add an error to the model state
+        ModelState.AddModelError("Login", "Invalid login attempt");
+
+        // if we got this far, something failed, redisplay form
+        return Page();
+    }
+}
+
+public class LoginViewModel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
